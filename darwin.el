7c4f9b147c6e10ad44dd013278b5c53d60db6e90
Donald Ephraim Curtis <dcurtis@milkbox.net>
2011-04-15 22:05:53 -0500

    add ack.el

--- darwin.el
+++
@@ -1,4 +0,0 @@
-(setenv "PYTHONPATH" "/Users/dcurtis/Development/compepi:/Users/dcurtis/Development/networkx")
-(set-face-font 'default "Menlo")
-;;(set-face-font 'default "Monaco")
-(provide 'milkmacs-darwin)
