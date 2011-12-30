7faf051b4186b5f4cba94e0408b0eaf29bab42ca
Donald Ephraim Curtis <dcurtis@milkbox.net>
2011-12-30 10:26:00 -0600

    fix authorized themes

--- custom.el
+++ custom.el
@@ -16,7 +16,7 @@
  '(cua-mode nil nil (cua-base))
  '(cursor-color "#657b83")
  '(custom-enabled-themes (quote (molokai)))
- '(custom-safe-themes (quote ("dd29a555206a9e795abb18e7c15557d68b54ded0" default)))
+ '(custom-safe-themes (quote ("1aa582db8e9d8a860300f7b45fb58322695ad5cb" default)))
  '(custom-theme-directory "~/.emacs.d/themes/")
  '(default-frame-alist (quote ((font . "Inconsolata-13"))))
  '(deft-directory "/Users/dcurtis/Dropbox/Elements")
