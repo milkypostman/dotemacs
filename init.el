e483f4fa1aa737f3736fa0a5bc8c945b293a62a0
Donald Curtis <dcurtis@milkbox.net>
2012-05-07 10:53:16 -0500

    Cleanup commented code

--- init.el
+++ init.el
@@ -244,14 +244,7 @@
      (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
      (setq reftex-plug-into-AUCTeX t)
      (define-key TeX-mode-map (kbd "C-c C-m") 'TeX-command-master)
-     (define-key TeX-mode-map (kbd "C-c C-c") 'TeX-compile)
-     ;; (define-key TeX-mode-map (kbd "C-c C-c")
-     ;;   (lambda ()
-     ;;     (interactive)
-     ;;     (TeX-save-document (TeX-master-file))
-     ;;     (TeX-command "LaTeX" 'TeX-master-file)
-     ;;     ))
-     ))
+     (define-key TeX-mode-map (kbd "C-c C-c") 'TeX-compile)))
 
 (eval-after-load 'reftex
   '(progn
@@ -352,63 +345,7 @@
 (when window-system
   (menu-bar-mode t)
   ;; specify a unicode font : MENLO (forced normal)
-  (set-fontset-font "fontset-default" 'unicode "-apple-Menlo-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
-
-  ;;(set-fontset-font "fontset-default" 'unicode "DejaVu Sans Mono")
-  ;;(set-fontset-font "fontset-default" 'unicode "-apple-DejaVu_Sans_Mono-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
-  ;;(set-fontset-font "fontset-default" 'unicode '("Menlo" . "unicode-bmp"))
-
-  ;;(set-fontset-font "fontset-default" 'unicode "Arial Unicode MS-12")
-  )
-
-
-
-;; ;; Make a non-standard key binding.  We can put this in
-;; ;; c-mode-base-map because c-mode-map, c++-mode-map, and so on,
-;; ;; inherit from it.
-;; (defun my-c-initialization-hook ()
-;;   (define-key c-mode-base-map "\C-m" 'c-context-line-break))
-;; (add-hook 'c-initialization-hook 'my-c-initialization-hook)
-
-;; ;; offset customizations not in my-c-style
-;; ;; This will take precedence over any setting of the syntactic symbol
-;; ;; made by a style.
-;; (setq c-offsets-alist '((member-init-intro . ++)))
-
-;; ;; Create my personal style.
-;; (defconst my-c-style
-;;   '((c-tab-always-indent        . t)
-;;     (c-comment-only-line-offset . 4)
-;;     (c-hanging-braces-alist     . ((substatement-open after)
-;;                                    (brace-list-open)))
-;;     (c-hanging-colons-alist     . ((member-init-intro before)
-;;                                    (inher-intro)
-;;                                    (case-label after)
-;;                                    (label after)
-;;                                    (access-label after)))
-;;     (c-cleanup-list             . (scope-operator
-;;                                    empty-defun-braces
-;;                                    defun-close-semi))
-;;     (c-offsets-alist            . ((arglist-close . c-lineup-arglist)
-;;                                    (substatement-open . 0)
-;;                                    (case-label        . 4)
-;;                                    (block-open        . 0)
-;;                                    (knr-argdecl-intro . -)))
-;;     (c-echo-syntactic-information-p . t))
-;;   "My C Programming Style")
-;; (c-add-style "PERSONAL" my-c-style)
-
-;; ;; Customizations for all modes in CC Mode.
-;; (defun my-c-mode-common-hook ()
-;;   ;; set my personal style for the current buffer
-;;   (c-set-style "PERSONAL")
-;;   ;; other customizations
-;;   (setq tab-width 8
-;;         ;; this will make sure spaces are used instead of tabs
-;;         indent-tabs-mode nil)
-;;   ;; we like auto-newline, but not hungry-delete
-;;   (c-toggle-auto-newline 1))
-;; (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+  (set-fontset-font "fontset-default" 'unicode "-apple-Menlo-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1"))
 
 
 
