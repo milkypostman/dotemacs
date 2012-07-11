99c2b0c84eb28b0e8cb7776091af7cc416304d94
Donald Curtis <dcurtis@milkbox.net>
2012-07-11 17:47:10 -0500

    fix multiple-cursors

--- init.el
+++ init.el
@@ -365,7 +365,9 @@
 (global-set-key (kbd "C-M-m") 'mark-more-like-this)
 (global-set-key (kbd "C-*") 'mark-all-like-this)
 
-(global-set-key (kbd "C-S-c C-S-c") 'mc/add-multiple-cursors-to-region-lines)
+
+;;; multiple-cursors
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
 (global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
 
