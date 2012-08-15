c088d773624c384cc317ead60fabc2fb4f421a7b
Donald Curtis <dcurtis@milkbox.net>
2012-08-15 17:47:23 -0500

    fix auto-completion include ropemacs!

--- init.el
+++ init.el
@@ -607,6 +607,31 @@
       ein:notebook-console-executable (expand-file-name "~/.virtualenv/default/bin/ipython")
       ein:notebook-console-security-dir (expand-file-name "~/.ipython/profile_nbserver/security"))
 
+(after 'auto-complete
+       (add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
+       (setq ac-use-menu-map t)
+       (define-key ac-menu-map "\C-n" 'ac-next)
+       (define-key ac-menu-map "\C-p" 'ac-previous))
+
+(after 'auto-complete-config
+       (ac-config-default)
+       (when (file-exists-p (expand-file-name "/Users/dcurtis/.emacs.d/elisp/Pymacs"))
+         (ac-ropemacs-initialize)
+         (ac-ropemacs-setup)))
+
+(after 'auto-complete-autoloads
+       (autoload 'auto-complete-mode "auto-complete" "enable auto-complete-mode" t nil)
+       (add-hook 'python-mode-hook
+                 (lambda ()
+                   (require 'auto-complete-config)
+                   (add-to-list 'ac-sources 'ac-source-ropemacs)
+                   (auto-complete-mode))))
+
+
+(setq ropemacs-enable-autoimport t)
+(add-to-list 'load-path "/Users/dcurtis/.emacs.d/elisp/Pymacs")
+
+
 
 (defun python-modes-init ()
   "initialization for all python modes"
