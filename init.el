56863ba105fd0fc065f85a5159e4f1d69c908bf6
Donald Curtis <dcurtis@milkbox.net>
2012-08-15 16:32:48 -0500

    add variables for ipython and ein modes

--- init.el
+++ init.el
@@ -595,6 +595,19 @@
 ;;; python
 (add-font-lock-numbers 'python-mode)
 
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args ""
+      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+      python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+      python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
+      python-shell-completion-module-string-code "';'.join(module_completion('''%s'''))\n"
+      python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+(setq ein:use-auto-complete t
+      ein:notebook-console-executable (expand-file-name "~/.virtualenv/default/bin/ipython")
+      ein:notebook-console-security-dir (expand-file-name "~/.ipython/profile_nbserver/security"))
+
+
 (defun python-modes-init ()
   "initialization for all python modes"
   ;; (setup-virtualenv)
