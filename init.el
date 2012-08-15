530065ed2a4707e1047bab615b596fd9072eff8d
Donald Curtis <dcurtis@milkbox.net>
2012-08-15 16:32:32 -0500

    remove more-like-this return binding

--- init.el
+++ init.el
@@ -421,7 +421,6 @@
 (global-set-key (kbd "C-x r t") 'inline-string-rectangle)
 (global-set-key (kbd "C-<") 'mark-previous-like-this)
 (global-set-key (kbd "C->") 'mark-next-like-this)
-(global-set-key (kbd "C-M-m") 'mark-more-like-this)
 (global-set-key (kbd "C-*") 'mark-all-like-this)
 
 
