1b1c9927e6e8bde5614614dc136bb3d0d2a0d474
Donald Curtis <dcurtis@milkbox.net>
2012-10-07 21:51:12 -0500

    add cleanup-buffer function

--- defun.el
+++ defun.el
@@ -417,6 +417,22 @@ Format VAL for COLWIDTH column and specified ALIGN."
   (interactive)
   (indent-region (point-min) (point-max)))
 
+(defun cleanup-buffer-safe ()
+  "Perform a bunch of safe operations on the whitespace content of a buffer.
+Does not indent buffer, because it is used for a before-save-hook, and that
+might be bad."
+  (interactive)
+  (untabify-buffer)
+  (delete-trailing-whitespace)
+  (set-buffer-file-coding-system 'utf-8))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer.
+Including indent-buffer, which should not be called automatically on save."
+  (interactive)
+  (cleanup-buffer-safe)
+  (indent-buffer))
+
 (defun other-window-reverse ()
   "Select the other window but in reverse."
   (interactive)
