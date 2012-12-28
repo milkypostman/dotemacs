e112cebcaf2c238ed1325129bfc95f48e2d0d5a1
Donald Curtis <dcurtis@milkbox.net>
2012-12-28 08:17:05 -0600

    magit fixes

--- init.el
+++ init.el
@@ -575,6 +575,39 @@
 ;;; magit
 (global-set-key (kbd "C-x g") 'magit-status)
 
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restore the previous window configuration and kill the magit buffer."
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
+
 
 
 ;;; deft
@@ -878,4 +911,3 @@
 ;; time-stamp-start: "Updated: +"
 ;; time-stamp-end: "$"
 ;; End:
-
