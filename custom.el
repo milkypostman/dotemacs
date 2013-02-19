a97648c217e5184ba6ee1bdb6a99520e52764a39
Donald Curtis <dcurtis@milkbox.net>
2013-02-19 10:18:45 -0600

    update custom variables

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-term-color-vector [unspecified "#1F1611" "#660000" "#144212" "#EFC232" "#5798AE" "#BE73FD" "#93C1BC" "#E6E1DC"])
  '(custom-enabled-themes (quote (base16-railscasts)))
- '(custom-safe-themes (quote ("f41fd682a3cd1e16796068a2ca96e82cfd274e58b978156da0acce4d56f2b0d5" "51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" "ae8d0f1f36460f3705b583970188e4fbb145805b7accce0adb41031d99bd2580" default)))
+ '(custom-safe-themes (quote ("41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "978ff9496928cc94639cb1084004bf64235c5c7fb0cfbcc38a3871eb95fa88f6" "f41fd682a3cd1e16796068a2ca96e82cfd274e58b978156da0acce4d56f2b0d5" "51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" "ae8d0f1f36460f3705b583970188e4fbb145805b7accce0adb41031d99bd2580" default)))
  '(ede-project-directories (quote ("/Users/dcurtis/src/Arduino-Makefile/examples/HelloWorld")))
  '(fci-rule-character-color "#452E2E")
  '(fci-rule-color "#2a2a2a")
