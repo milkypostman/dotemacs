da394ea9a16e10cbe6e2f8b21952c92fb5f38905
Donald Curtis <dcurtis@milkbox.net>
2013-08-29 22:52:24 -0700

    All

--- init.el
+++ init.el
@@ -1897,7 +1897,6 @@ Including indent-buffer, which should not be called automatically on save."
 
 
 ;;; customizations
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
