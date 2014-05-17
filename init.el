d8cd883b91adcdef0a3626f6ab9401a6a0087256
Donald Curtis <d@milkbox.net>
2014-05-17 00:15:51 -0400

    setup evil

--- init.el
+++ init.el
@@ -745,17 +745,24 @@ mouse-1: Display Line and Column Mode Menu")))))))
 
 
 ;;;; evil-mode
+(after "evil-autoloads"
+  (setq evil-toggle-key "C-x C-z")
+  (evil-mode 1))
+
 (after 'evil
   (define-key evil-normal-state-map (kbd "C-y") 'evil-paste-before)
+  (evil-set-initial-state 'git-commit-mode 'insert)
   ;; (define-key evil-ex-map "e " 'ido-find-file)
   ;; (define-key evil-ex-map "w " 'ido-write-file)
   ;; (define-key evil-ex-map "b " 'ido-switch-buffer)
   (when (boundp 'global-surround-mode) (global-surround-mode))
   (ignore-errors (require 'evil-leader)))
 
+
 ;;;; evil-leader
 (after 'evil-leader
-  (evil-leader/set-key (kbd "t") 'find-file-in-project))
+  (evil-leader/set-key (kbd "t") 'find-file-in-project)
+  (evil-leader/set-key (kbd "g") 'magit-status))
 
 
 ;;;; hl-sentence
